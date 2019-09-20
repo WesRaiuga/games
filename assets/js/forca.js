6507e0c443b181108f0758623f2c8883e850bef7
WesRaiuga <weslley_wenny@hotmail.com>
2019-09-19 22:05:41 -0300

    Jogo da Forca: chances, botões das letras e resposta.

--- assets/js/forca.js
+++ assets/js/forca.js
@@ -1,6 +1,10 @@
 function Dom() {
+    this.chances = document.querySelector('#chances');
     this.tema = document.querySelector('#tema');
     this.palavra = document.querySelector("#palavra");
+    this.resposta = document.querySelector('#resposta');
+    this.letras = document.querySelector("#letras");
+    // this.botoesLetras;
 
     this.criaElemento = (tagName, className, content) => {
         const elem = document.createElement(tagName);
@@ -19,9 +23,20 @@ function Dom() {
             }
         }
     }
+
+    this.carregarBotoesLetras = () => {
+        this.letras.innerHTML = '';
+        const alfabeto = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+        for (let i = 0; i < alfabeto.length; i++) {
+            this.letras.appendChild( 
+                this.criaElemento( 'button', 'btn-letra',alfabeto[i] )
+            );
+        }
+    }
 }
 
 function Configuracoes() {
+    this.chances = 6;
     this.dicionario = [
         {
             "tema": "Games",
@@ -51,8 +66,11 @@ function Jogo() {
     this.palavraAtual = new Palavra(this.config.dicionario);
 
     this.carregarElementosNaTela = () => {
+        this.dom.chances.innerHTML = this.config.chances;
         this.dom.tema.innerHTML = this.palavraAtual.tema.name;
         this.dom.carregarLacunas(this.palavraAtual.name);
+        this.dom.resposta.innerHTML = this.palavraAtual.name;
+        this.dom.carregarBotoesLetras();
     };
     
     this.start = () => {
