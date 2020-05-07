ce4d9bd5099700e9566b2f16bd83ae6b852383b3
wesraiuga <weslley_wenny@hotmail.com>
2020-05-07 14:50:40 -0300

    Jokenpo: Atualização no botão start

--- assets/js/jokenpo.js
+++ assets/js/jokenpo.js
@@ -66,7 +66,7 @@ const darFeedbackEscolha = (mostrar, apagar1, apagar2, cor) => {
 
 const habilitarStart = () => {
     start.disabled = false;
-    start.style = 'cursor: pointer; background-color: #1ba8e9;';
+    start.style = 'cursor: pointer; background-color: #1ba8e9; visibility: visible;';
 };
 
 const resetarBotoes = () => {
